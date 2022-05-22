1830ad65fb7c83a24a59e90119c72dd53911f6db
taslima63 <ruba6315@gmail.com>
2022-05-22 18:10:52 +0900

    dashboard nested route added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddProduct from './Pages/Dashboard/AddProduct';
+import AddReview from './Pages/Dashboard/AddReview';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import ManageOrder from './Pages/Dashboard/ManageOrder';
+import ManageProducts from './Pages/Dashboard/ManageProducts';
+import MyOrders from './Pages/Dashboard/MyOrders';
+import MyProfile from './Pages/Dashboard/MyProfile';
+import Users from './Pages/Dashboard/Users';
 import About from './Pages/Home/About';
 import Contact from './Pages/Home/Contact';
 import Home from './Pages/Home/Home';
 import Reviews from './Pages/Home/Reviews';
 import SignIn from './Pages/Login/SignIn';
-import Login from './Pages/Login/SignIn';
 import Signup from './Pages/Login/Signup';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
@@ -23,6 +30,19 @@ function App() {
         <Route path='contact' element={<Contact />}></Route>
         <Route path='signin' element={<SignIn />}></Route>
         <Route path='signup' element={<Signup />}></Route>
+
+        <Route path='dashboard' element={
+          <Dashboard />
+        }
+        >
+          <Route index element={<MyProfile />}></Route>
+          <Route path="myOrder" element={<MyOrders />}></Route>
+          <Route path="addReview" element={<AddReview />}></Route>
+          <Route path="manageOrder" element={<ManageOrder />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="addProduct" element={<AddProduct />}></Route>
+          <Route path="manageProduct" element={<ManageProducts />}></Route>
+        </Route>
       </Routes>
       <Footer />
     </div>
---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+
+
+
+const Dashboard = () => {
+
+    return (
+        <div className="drawer drawer-mobile">
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content ">
+                <h2 className='text-3xl font-bold text-yellow-500 text-center'>Welcome to Dashboard</h2>
+                <Outlet />
+
+
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+
+                    <li><Link to="/dashboard">My Orders</Link></li>
+                    <li><Link to="/dashboard/addReview">Add Reviews</Link></li>
+                    <li><Link to="/dashboard/myProfile">My Profile</Link></li>
+                    <li><Link to="/dashboard/users">Manage Admin List</Link></li>
+                    <li><Link to="/dashboard/manageOrder">Manage Orders</Link></li>
+                    <li><Link to="/dashboard/addProduct">Add Product</Link></li>
+                    <li><Link to="/dashboard/manageProduct">Manage Products</Link></li>
+
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
